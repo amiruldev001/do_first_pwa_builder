71b589798189b8c60268d6519c34a37d749b385d
amiruldev001 <amirul.dev001@gmail.com>
2026-01-06 16:20:44 +0800

    Add ComponentRegistry with Text and Button components

--- js/components.js
+++ js/components.js
@@ -1,1 +1,24 @@
+window.ComponentRegistry = {
+  Text: {
+    defaultProps: {
+      content: "Text"
+    },
+    render: (props) => {
+      const el = document.createElement("div");
+      el.innerText = props.content;
+      return el;
+    }
+  },
+
+  Button: {
+    defaultProps: {
+      label: "Click Me"
+    },
+    render: (props) => {
+      const el = document.createElement("button");
+      el.innerText = props.label;
+      return el;
+    }
+  }
+};
 
