61e95f878bfff9f925bac24cdd415780e42c6967
amiruldev001 <amirul.dev001@gmail.com>
2026-01-06 16:21:03 +0800

    Add renderPage function to render components

--- js/renderer.js
+++ js/renderer.js
@@ -1,1 +1,13 @@
+window.renderPage = function () {
+  const dropzone = document.getElementById("dropzone");
+  dropzone.innerHTML = "";
+
+  const components = appState.pages[appState.currentPage];
+
+  components.forEach(comp => {
+    const def = ComponentRegistry[comp.type];
+    const el = def.render(comp.props);
+    dropzone.appendChild(el);
+  });
+};
 
