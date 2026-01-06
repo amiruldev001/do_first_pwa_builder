49d7b71918ed340b23abecf36498ec41e2f528df
amiruldev001 <amirul.dev001@gmail.com>
2026-01-06 16:21:21 +0800

    Add drag-and-drop support for components
    Implement drag-and-drop functionality for components.

--- js/builder.js
+++ js/builder.js
@@ -1,1 +1,22 @@
+document.querySelectorAll(".component").forEach(item => {
+  item.addEventListener("dragstart", e => {
+    e.dataTransfer.setData("type", e.target.dataset.type);
+  });
+});
+
+const dropzone = document.getElementById("dropzone");
+
+dropzone.addEventListener("dragover", e => e.preventDefault());
+
+dropzone.addEventListener("drop", e => {
+  e.preventDefault();
+  const type = e.dataTransfer.getData("type");
+
+  appState.pages.home.push({
+    type,
+    props: { ...ComponentRegistry[type].defaultProps }
+  });
+
+  renderPage();
+});
 
